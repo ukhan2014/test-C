d6964727cc71cd0b43d1c53f2c76a4d4b16b60f3
cabanaboi <usman.na.khan@gmail.com>
2015-10-23 02:07:15 -0700

    added create node function. Tried to add hookupNode function (descr in comments). Need to think about hookup function on paper to see how it should be implemented

--- BST.cpp
+++ BST.cpp
@@ -1,4 +1,4 @@
- ,#include <stdio.h>
+#include <stdio.h>
 
 typedef struct bstNode {
     int data;
@@ -11,14 +11,45 @@ bstNode_t * lookup (int data, bstNode_t * tree) {
     bstNode_t * node = tree;
     if(node->data == data)
       found = true;
-    else if (node->)
+    else if (data < node->data) {
+    	return lookup(data, node->left);
+    }
+    else if (data > node->data) {
+    	return lookup(data, node->right);
+    }
     if(found)
       return node;
     else
       return NULL;
 }
 
+//create and return address of a new node
+//arg: data to put in new node
+bstNode_t * createNode(int data) {
+	bstNode_t node;
+	node.data = data;
+	return &node;
+}
+
+//hook up pointers to a node
+//if a certain node from parent, LC, RC
+//doesn't exist, just put NULL
+void hookupNode(bstNode_t * parent, bstNode * leftChild,
+		bstNode_t * rightChild, bstNode_t * node) {
+	bstNode_t * temp;
+	if(node->data < parent->data) {
+		temp = parent->left;
+		parent->left = node;
+		node->left = temp;
+	}
+	else if(node->data > parent->data) {
+		temp = parent->right;
+		parent->right = node;
+		node->right = temp;
+	}
+}
+
 int main () {
-    
+	printf("built fine\n");
     return 0;
 }
