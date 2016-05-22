ecead77b9c07780a8d5ec80c70fae29aac0adf6f
cabanaboi <ukhan2014@users.noreply.github.com>
2016-05-22 14:42:26 -0700

    replaced list sequence with vector
    Lists are too slow for sequences that are long.
    
    Also, the list of sequences itself is a list, so it still may be too slow for this challenge.

--- darray.c
+++ darray.c
@@ -1,31 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define VECTOR_INITIAL_CAPACITY (100)
 
-
-
-typedef struct seqNode {
-    int data;
-    struct seqNode * next;
-} seqNode_t;
+// Define a vector type
+typedef struct {
+    int size;      // slots used so far
+    int capacity;  // total available slots
+    int *data;     // array of integers we're storing
+} Vector;
 
 typedef struct listNode {
-    seqNode_t * seq;
+    Vector * seq;
     struct listNode * next;
 } listNode_t;
 
-seqNode_t * createSeqNode() {
-    seqNode_t * seq = (seqNode_t *)malloc(sizeof(seqNode_t));
-    seq->data = 0;
-    seq->next = NULL;
-    return seq;
+void vector_init(Vector *vector) {
+    vector->size = 0;
+    vector->capacity = VECTOR_INITIAL_CAPACITY;
+    vector->data = (int *)malloc(sizeof(Vector));
 }
 
-listNode_t * createListNode() {
-    listNode_t * list = (listNode_t *)malloc(sizeof(listNode_t));
-    list->next = NULL;
-    list->seq = NULL;
-    return list;
+void vector_double_capacity_if_full(Vector **vector){
+    if((*vector)->size >= (*vector)->capacity) {
+        printf("capacity before is %d\n", (*vector)->capacity);
+        (*vector)->capacity *= 2;
+        printf("capacity after multiply is %d\n", (*vector)->capacity);
+        (*vector)->data = (int *)realloc((*vector)->data, sizeof(int)*((*vector)->capacity));
+    }
+}
+
+void vector_append(Vector *vector, int value){
+    vector_double_capacity_if_full(&vector);
+    *((vector->data) + vector->size) = value;
+    (vector->size)++;
+}
+
+void vector_free(Vector *vector){
+    free(vector->data);
 }
 
 int main() {
@@ -55,7 +67,6 @@ int main() {
         //printf("queryType=%d  x=%d  y=%d\n", queryType,x,y);
         //queryType=1; x=0; y=5;
         int index = (x ^ lastAns) % N;
-        //printf("index=%d   Resetting to beginning of seqList\n",index);
         listPtr = seqList;                          // reset pointer to beginning of seqList
         for(i=0; i<index; i++) {
             listPtr = listPtr->next;                // traverse to index in seqList
@@ -63,48 +74,33 @@ int main() {
         
         if(queryType == 1) {
             // if this sequence is empty
-            seqNode_t * tempSeqPtr = NULL;          // temp Ptr to traverse a sequence
             if(!listPtr->seq) {
                 //printf("Creating new sequence\n");
-                listPtr->seq = createSeqNode();
-                listPtr->seq->data = y;
-                listPtr->seq->next = NULL;
+                listPtr->seq = (Vector *) malloc(sizeof(Vector));
+                vector_init(listPtr->seq);
+                vector_append(listPtr->seq, y);
             }
             else {
-                tempSeqPtr = listPtr->seq;
-                while(tempSeqPtr->next) {
-                    tempSeqPtr = tempSeqPtr->next;
-                }
-                tempSeqPtr->next = createSeqNode();
-                tempSeqPtr = tempSeqPtr->next;
-                tempSeqPtr->data = y;
-                tempSeqPtr->next = NULL;
+                vector_append(listPtr->seq, y);
             }
         }
         else if(queryType == 2) {
-            int size = 0;
-            seqNode_t * tempSeqPtr = listPtr->seq;
-            // find size of sequence
-            while(tempSeqPtr) {
-                tempSeqPtr = tempSeqPtr->next;
-                size++;
-            }
-            
-            int element = y % size;
-            tempSeqPtr = listPtr->seq;
-            // go to index element
-            int i;
-            for(i=0; i<element; i++) {
-                tempSeqPtr = tempSeqPtr->next;
-            }
-            lastAns = tempSeqPtr->data;
+            int element = y % (listPtr->seq->size);
+            lastAns = *((listPtr->seq->data)+element);
             printf("%d\n", lastAns);
         }
         else {
             break;
         }
-        
     }
-        
+    
+    // Free all memory allocated to vectors and listNodes
+    while(seqList) {
+        if(seqList->seq)
+            vector_free(seqList->seq);
+        listNode_t * temp = seqList;
+        seqList = seqList->next;
+        free(temp);
+    }
     return 0;
 }
