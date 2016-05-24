3b63a2c1d0e7461a46d69fad190bcbc3416dff8a
cabana <usman.na.khan@gmail.com>
2016-05-24 13:12:07 -0700

    adding an insert method

--- linked-list/linked-list/linked-list.c
+++ linked-list/linked-list/linked-list.c
@@ -7,6 +7,7 @@
 //
 
 #include "linked-list.h"
+#include <stdlib.h>
 
 
 /*
@@ -25,4 +26,26 @@ void Print(Node *head)
             tempPtr = tempPtr->next;
         }
     }
+}
+
+/*
+ Insert Node at the end of a linked list
+ head pointer input could be NULL as well for empty list
+ Return the head of the updated list
+ */
+Node* Insert(Node *head,int data)
+{
+    Node * tempPtr = head;
+    if(tempPtr) {
+        while(tempPtr->next) {
+            tempPtr = tempPtr->next;
+        }
+        tempPtr->next = (Node *) malloc(sizeof(Node));
+        tempPtr->next->data = data;
+    }
+    else {
+        head = (Node *) malloc(sizeof(Node));
+        head->data = data;
+    }
+    return head;
 }
