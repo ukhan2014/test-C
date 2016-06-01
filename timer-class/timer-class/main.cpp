7362b196dbbf82b0f3965e7c05c4ed66ce7098ae
cabana <usman.na.khan@gmail.com>
2016-06-01 01:45:44 -0700

    adding a simple-timer class to play with cpp classes

--- timer-class/timer-class/main.cpp
+++ timer-class/timer-class/main.cpp
@@ -7,9 +7,17 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include "timer.h"
+int main()
+{
+    timer aTimer;
+    aTimer.Start();
+    for( int i = 1; i < 10; i++ )
+    {
+        std::cout << i << std::endl;
+        while( aTimer.GetTicks() < 1000000 );
+        aTimer.Reset();
+    }
+    std::cout << "FIRE!" << std::endl;
     return 0;
 }
--- timer-class/timer-class/timer.h
+++ timer-class/timer-class/timer.h
@@ -1,13 +1,85 @@
-//
-//  timer.h
-//  timer-class
-//
-//  Created by Cabana on 6/1/16.
-//  Copyright © 2016 Cabana. All rights reserved.
-//
-
-#ifndef timer_h
-#define timer_h
-
-
-#endif /* timer_h */
+#ifndef TIMER_H
+#define TIMER_H
+#include <ctime>
+class timer
+{
+    clock_t startedAt;
+    clock_t pausedAt;
+    bool started;
+    bool paused;
+public:
+    timer();
+    bool IsStarted();
+    bool IsStopped();
+    bool IsPaused();
+    bool IsActive();
+    void Pause();
+    void Resume();
+    void Stop();
+    void Start();
+    void Reset();
+    clock_t GetTicks();
+};
+timer::timer()
+{
+    startedAt = 0;
+    pausedAt = 0;
+    paused = false;
+    started = false;
+}
+bool timer::IsStarted()
+{
+    return started;
+}
+bool timer::IsStopped()
+{
+    return !started;
+}
+bool timer::IsPaused()
+{
+    return paused;
+}
+bool timer::IsActive()
+{
+    return !paused & started;
+}
+void timer::Pause()
+{
+    if( paused || !started )
+        return;
+    paused = true;
+    pausedAt = clock();
+}
+void timer::Resume()
+{
+    if( !paused )
+        return;
+    paused = false;
+    startedAt += clock() - pausedAt;
+}
+void timer::Stop()
+{
+    started = false;
+}
+void timer::Start()
+{
+    if( started )
+        return;
+    started = true;
+    paused = false;
+    startedAt = clock();
+}
+void timer::Reset()
+{
+    paused = false;
+    startedAt = clock();
+}
+clock_t timer::GetTicks()
+{
+    if( !started )
+        return 0;
+    if( paused )
+        return pausedAt - startedAt;
+    return clock() - startedAt;
+}
+#endif
