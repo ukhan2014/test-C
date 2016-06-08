14f53eee6fdbf349fc11920de539f23cefb37bc5
cabana <usman.na.khan@gmail.com>
2016-06-07 23:43:22 -0700

    trying to print the powerset of a set

---
+++ subsets/subsets/main.c
@@ -0,0 +1,36 @@
+//
+//  main.c
+//  subsets
+//
+//  Created by Cabana on 6/7/16.
+//  Copyright © 2016 Cabana. All rights reserved.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    
+    int set[] = {1,2,3,4,5,6,7,8,9,10};
+    
+    int set_size = sizeof(set)/sizeof(int);
+    int powerset_size = pow(2,set_size);
+    
+    
+    printf("powerset_size = %d\n", powerset_size);
+    printf("set_size = %d\n", set_size);
+    int i, j;
+    
+    for(i = 0; i < powerset_size; i++) {
+        for(j = 0; j < set_size; j++) {
+            if(i >> j) {
+                printf("%d ", set[j]);
+            }
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
