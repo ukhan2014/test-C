a9a3ffe4d772af04615e59bdc97d35fb2f972460
cabana <usman.na.khan@gmail.com>
2016-06-08 21:16:31 -0700

    added logic for finding subsets that add up to a specific target value

--- subsets/subsets/main.c
+++ subsets/subsets/main.c
@@ -12,10 +12,12 @@
 #define BIT_0       (0x01)
 
 int main(int argc, const char * argv[]) {
+    
     // insert code here...
     printf("Hello, World!\n");
     
-    int set[] = {1,2,3,4,5};
+    int set[] = {1,2,3,4,5};    // the original set
+    int target = 5;         // the number subsets should add up to
     
     int set_size = sizeof(set)/sizeof(int);
     int powerset_size = pow(2,set_size);
@@ -23,16 +25,31 @@ int main(int argc, const char * argv[]) {
     
     printf("powerset_size = %d\n", powerset_size);
     printf("set_size = %d\n", set_size);
-    int i, j;
+    int i, j, sum;
     
-    for(i = 1; i < powerset_size + 1; i++) {
+    for(i = 1; i < powerset_size; i++) {
+        // initialize sum to 0
+        sum = 0;
+        
         printf("set %d = ", i);
         for(j = 0; j < set_size; j++) {
             if((i >> j)&BIT_0) {
+                sum += set[j];
                 printf("%d ", set[j]);
             }
         }
+        
         printf("\n");
+        
+        if(sum == target) {
+            printf("sum found!\n");
+            // find hamming weight of i (# of set bits in i)
+            
+            // allocate memory (of size = hamming weight of i)
+            
+            // assign to next index in our int * array[100]
+            
+        }
     }
     
     return 0;
