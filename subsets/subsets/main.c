f148ef9733f86c981c319896602813617062480a
cabana <usman.na.khan@gmail.com>
2016-06-08 20:51:19 -0700

    fixed, actually finds all subsets in powerset now

--- subsets/subsets/main.c
+++ subsets/subsets/main.c
@@ -9,11 +9,13 @@
 #include <stdio.h>
 #include <math.h>
 
+#define BIT_0       (0x01)
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
     
-    int set[] = {1,2,3,4,5,6,7,8,9,10};
+    int set[] = {1,2,3,4,5};
     
     int set_size = sizeof(set)/sizeof(int);
     int powerset_size = pow(2,set_size);
@@ -23,9 +25,10 @@ int main(int argc, const char * argv[]) {
     printf("set_size = %d\n", set_size);
     int i, j;
     
-    for(i = 0; i < powerset_size; i++) {
+    for(i = 1; i < powerset_size + 1; i++) {
+        printf("set %d = ", i);
         for(j = 0; j < set_size; j++) {
-            if(i >> j) {
+            if((i >> j)&BIT_0) {
                 printf("%d ", set[j]);
             }
         }
