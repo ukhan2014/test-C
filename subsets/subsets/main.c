c6ab33c26b2421e8a0b72e2b8f5cd985789699d7
cabana <usman.na.khan@gmail.com>
2016-06-08 21:47:40 -0700

    added variable hits to count how many subsets we have found that match criteria. Added for loop to fill up subsets and another for loop to print them out at the end.

--- subsets/subsets/main.c
+++ subsets/subsets/main.c
@@ -19,7 +19,7 @@
 
 int main(int argc, const char * argv[]) {
     int * subsets[100];         // array of 100 pointers to int
-    int subset_index = 0;
+    int subsets_index = 0;
     int set[] = {1,2,3,4,5};    // the original set
     int target = 5;             // the number subsets should add up to
     
@@ -32,6 +32,7 @@ int main(int argc, const char * argv[]) {
     int j;
     int sum;        // variable to hold sum of subsets
     int ham;        // variable to hold hamming weight (# of set bits)
+    int hits = 0;       // how many subsets' sum is the target value t?
     
     for(i = 1; i < powerset_size; i++) {
         sum = 0;
@@ -48,15 +49,31 @@ int main(int argc, const char * argv[]) {
         printf("  ham = %d \n", ham);
         
         if(sum == target) {
+            hits++;
             printf("sum found!\n");
             
             // allocate memory (of size = hamming weight of i)
             int * correct_sum_subset = (int *) malloc(sizeof(ham * sizeof(int)));
             
+            int new_subset_index = 0;
+            // fill correct_sum_subset
+            for(j = 0; j < set_size; j++) {
+                if((i >> j)&BIT_0) {
+                     correct_sum_subset[new_subset_index++] = set[j];
+                }
+            }
             // assign to next index in our int * array[100]
-            subsets[subset_index++] = correct_sum_subset;
+            subsets[subsets_index++] = correct_sum_subset;
             
         }
+    }
+    
+    printf("Subsets that meet criteria:\n");
+    for(i=0; i<hits; i++) {
+        for(j=0; j<sizeof(subsets[i]); j++) {
+            printf("%d ", subsets[i][j]);
+        }
+        printf("\n");
     }
     
     return 0;
