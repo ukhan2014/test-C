70cb699c7212e93d55a0ed8a7fc3a2af74f81ba5
cabana <usman.na.khan@gmail.com>
2016-06-09 22:34:53 -0700

    fixed spacing issue

--- subsets/subsets/main.c
+++ subsets/subsets/main.c
@@ -46,7 +46,7 @@ int main(int argc, const char * argv[]) {
             }
         }
         
-        printf("  ham = %d \n", hamming_weight);
+        printf("\tham = %d \n", hamming_weight);
         
         if(sum == target) {
             subsets_found++;
