61bf1b7b2b57e40f5392a8dedc1c535895971121
cabana <usman.na.khan@gmail.com>
2016-06-10 21:36:06 -0700

    huffman tree node struct

--- trees/trees/trees.h
+++ trees/trees/trees.h
@@ -11,12 +11,23 @@
 
 #include <stdio.h>
 
+// Node of a general tree
 typedef struct node
 {
     int data;
     struct node * left;
     struct node * right;
 } node;
+
+// Node of a huffman tree
+typedef struct huffman_node
+{
+    int freq;
+    char data;
+    struct huffman_node * left;
+    struct huffman_node * right;
+    
+} huffnode;
 
 void preOrder(node * root);
 void postOrder(node * root);
